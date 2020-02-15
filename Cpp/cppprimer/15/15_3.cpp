6fa262e1be2829cc49f1bdd52de566b0c831a4b7
ms303956362 <ms303956362@163.com>
2020-02-15 15:52:03 +0800

    20200215-5

--- Cpp/cppprimer/15/15_3.cpp
+++ Cpp/cppprimer/15/15_3.cpp
@@ -10,18 +10,17 @@ using std::string;
 
 int main(int argc, char const *argv[])
 {
-    // 15.3
+    // 15.11
     // MyQuote mq("0-201-70353-X", 24.99);
-    // print_total(cout, mq, 3);
-
-    // 15.5
     // MyBulkQuote mbq("0-399-82477-1", 45.39, 3, 0.8);
-    // print_total(cout, mbq, 2);
-    // print_total(cout, mbq, 3);
+    // MyBulkQuotemax mbqm("0-201-78345-X", 25.00, 2, 5, 0.7);
+    // mq.debug(cout) << endl;
+    // mbq.debug(cout) << endl ;
+    // mbqm.debug(cout) << endl;
 
-    // 15.7
-    // MyBulkQuotemax mbq("0-399-82477-1", 45.39, 3, 6, 0.8);
-    // print_total(cout, mbq, 7);
-
+    // 15.14
+    MyBulkQuote mbq("0-399-82477-1", 45.39, 3, 0.8);
+    MyQuote *pmq = &mbq;
+    pmq->debug(cout);
     return 0;
 }
