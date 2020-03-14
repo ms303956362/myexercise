66fed6bed680c509b71af8e687f93db5b30cc420
ms303956362 <ms303956362@163.com>
2020-03-15 01:03:42 +0800

    20200315-2

---
+++ DS/4_Stack/Queue.h
@@ -0,0 +1,9 @@
+#include "List.h"
+
+template<typename T>
+class Queue: public List<T> {
+    void enqueue(const T &e) { insertAsLast(e); }
+    T dequeue() { return remove(first()); }
+    T &front() { return first()->data; }
+    T &rear() { return last()->data; }
+}
