6491820babd52773c743eea04b8df775d65fc8ea
ms303956362 <ms303956362@163.com>
2020-06-04 17:59:16 +0800

    20200604-2

--- Python/IRL_Exercise/1/demo1.py
+++ Python/IRL_Exercise/1/demo1.py
@@ -1,6 +1,16 @@
 import gym
+import torch
+print(torch.__version__)
 env = gym.make('CartPole-v0')
 env.reset()
-for _ in range(1000):
-    env.render()
-    env.step(env.action_space.sample()) # take a random action
+# for _ in range(1000):
+#     env.render()
+#     env.step(env.action_space.sample()) # take a random action
+
+for i_episode in range(20):
+    observation = env.reset()
+    for t in range(100):
+        env.render()
+        print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
